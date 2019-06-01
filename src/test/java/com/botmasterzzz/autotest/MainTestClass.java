836f5c1a40d885f68318c0cbeabb46656f88a66c
Azat Akhunov <azat.akhunov@protonmail.com>
2019-06-01 15:12:19 +0300

    [CF] to web driver updated

--- src/test/java/com/botmasterzzz/autotest/MainTestClass.java
+++ src/test/java/com/botmasterzzz/autotest/MainTestClass.java
@@ -1,6 +1,5 @@
 package com.botmasterzzz.autotest;
 
-import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -10,7 +9,6 @@ public class MainTestClass extends TestBase{
     @Test
     public void step01(){
         String title = "Botmasterzzz - Главный портал по сервису и поддержке искусственного интеллекта";
-        WebDriver driver = applicationManager.getWebDriver();
         driver.navigate().to("https://botmasterzzz.com");
         assertThat(String.format("Заголовок главной страницы не соответствует '%s'", title), title.equals(driver.getTitle()));
     }
--- src/test/java/com/botmasterzzz/autotest/TestBase.java
+++ src/test/java/com/botmasterzzz/autotest/TestBase.java
@@ -1,26 +1,24 @@
 package com.botmasterzzz.autotest;
 
 import com.botmasterzzz.autotest.helpers.ApplicationManager;
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
 
 public abstract class TestBase {
 
     protected ApplicationManager applicationManager;
+    protected WebDriver driver;
 
     @BeforeClass
     protected void beforeClass(){
        this.applicationManager = ApplicationManager.getInstance();
+       driver = applicationManager.getWebDriver();
     }
 
     @AfterClass
     protected void afterClass(){
-        applicationManager.getWebDriver().close();
+        driver.close();
     }
 
-    @BeforeMethod
-    protected void beforeMethod(){
-        System.out.println(2);
-    }
 }
