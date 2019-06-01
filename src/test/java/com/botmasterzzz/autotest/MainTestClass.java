07938574805a634bab81d4e70fe03b0792c2f487
Azat Akhunov <azat.akhunov@protonmail.com>
2019-06-01 17:01:10 +0300

    [CF] dependencies was added

--- src/test/java/com/botmasterzzz/autotest/MainTestClass.java
+++ src/test/java/com/botmasterzzz/autotest/MainTestClass.java
@@ -1,27 +1,43 @@
 package com.botmasterzzz.autotest;
 
+import com.botmasterzzz.autotest.helpers.ApplicationManager;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Before;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeDriverService;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.*;
 
-
 import java.io.File;
+import java.io.IOException;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class MainTestClass extends TestBase{
 
+    private static ChromeDriverService service;
     private WebDriver driver;
+
+    @BeforeTest
+    public static void createAndStartService() throws IOException {
+        service = new ChromeDriverService.Builder()
+                .usingDriverExecutable(new File("/usr/bin/chromedriver"))
+                .usingAnyFreePort()
+                .build();
+        service.start();
+    }
+
+    @AfterTest
+    public static void createAndStopService() {
+        service.stop();
+    }
+
 
     @BeforeClass
     public void createDriver() {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("start-maximized");
-        options.addArguments("--headless");
-        options.setBinary(new File("/usr/bin/google-chrome"));
-        driver = new ChromeDriver(options);
+        driver = new RemoteWebDriver(service.getUrl(),
+                DesiredCapabilities.chrome());
     }
 
     @AfterClass
