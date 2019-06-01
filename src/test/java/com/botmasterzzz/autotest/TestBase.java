b1c38f84ab89750198819f7ea620c1cd854e5c96
Azat Akhunov <azat.akhunov@protonmail.com>
2019-06-01 15:37:14 +0300

    [CF] debug level

--- src/test/java/com/botmasterzzz/autotest/TestBase.java
+++ src/test/java/com/botmasterzzz/autotest/TestBase.java
@@ -1,7 +1,6 @@
 package com.botmasterzzz.autotest;
 
 import com.botmasterzzz.autotest.helpers.ApplicationManager;
-import com.botmasterzzz.autotest.helpers.WebDriverHelper;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -9,19 +8,17 @@ import org.testng.annotations.BeforeClass;
 public abstract class TestBase {
 
     protected ApplicationManager applicationManager;
-    protected WebDriverHelper webDriverHelper;
     protected WebDriver driver;
 
     @BeforeClass
     protected void beforeClass(){
        this.applicationManager = ApplicationManager.getInstance();
-       webDriverHelper = new WebDriverHelper(applicationManager);
-       driver = webDriverHelper.getDriver();
+       driver = this.applicationManager.getWebDriverHelper().getDriver();
     }
 
     @AfterClass
     protected void afterClass(){
-        this.webDriverHelper.close();
+        this.applicationManager.stopAll();
     }
 
 }
