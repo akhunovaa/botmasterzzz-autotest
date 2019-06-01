f6a7368e112294fe3e1b52626ab4d8c363440aba
Azat Akhunov <azat.akhunov@protonmail.com>
2019-06-01 15:24:02 +0300

    [CF] to web driver updated

--- src/test/java/com/botmasterzzz/autotest/MainTestClass.java
+++ src/test/java/com/botmasterzzz/autotest/MainTestClass.java
@@ -10,6 +10,7 @@ public class MainTestClass extends TestBase{
     public void step01(){
         String title = "Botmasterzzz -Главный портал по сервису и поддержке искусственного интеллекта";
         driver.navigate().to("https://botmasterzzz.com");
-        assertThat(String.format("Заголовок главной страницы не соответствует '%s'", title), title.equals(driver.getTitle()));
+        String pageTitile = driver.getTitle();
+        assertThat(String.format("Заголовок главной страницы не соответствует '%s'", title), title.equals(pageTitile));
     }
 }
--- src/test/java/com/botmasterzzz/autotest/TestBase.java
+++ src/test/java/com/botmasterzzz/autotest/TestBase.java
@@ -1,18 +1,27 @@
 package com.botmasterzzz.autotest;
 
 import com.botmasterzzz.autotest.helpers.ApplicationManager;
+import com.botmasterzzz.autotest.helpers.WebDriverHelper;
 import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 public abstract class TestBase {
 
     protected ApplicationManager applicationManager;
+    protected WebDriverHelper webDriverHelper;
     protected WebDriver driver;
 
     @BeforeClass
     protected void beforeClass(){
        this.applicationManager = ApplicationManager.getInstance();
-       driver = applicationManager.getWebDriver();
+       webDriverHelper = new WebDriverHelper(applicationManager);
+       driver = webDriverHelper.getDriver();
+    }
+
+    @AfterClass
+    protected void afterClass(){
+        this.webDriverHelper.close();
     }
 
 }
