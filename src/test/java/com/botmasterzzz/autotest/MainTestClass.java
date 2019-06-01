caaff11d290d65e61b663809ae270ed5b695cf62
Azat Akhunov <azat.akhunov@protonmail.com>
2019-06-01 15:40:34 +0300

    [CF] debug level

--- src/test/java/com/botmasterzzz/autotest/MainTestClass.java
+++ src/test/java/com/botmasterzzz/autotest/MainTestClass.java
@@ -1,10 +1,23 @@
 package com.botmasterzzz.autotest;
 
+import com.botmasterzzz.autotest.helpers.ApplicationManager;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 
 public class MainTestClass extends TestBase{
+
+    protected ApplicationManager applicationManager;
+    protected WebDriver driver;
+
+    @BeforeTest
+    public void beforeTest(){
+        this.applicationManager = ApplicationManager.getInstance();
+        driver = this.applicationManager.getWebDriverHelper().getDriver();
+    }
 
     @Test
     public void step01(){
@@ -12,5 +25,10 @@ public class MainTestClass extends TestBase{
         driver.navigate().to("https://botmasterzzz.com");
         String pageTitile = driver.getTitle();
         assertThat(String.format("Заголовок главной страницы не соответствует '%s'", title), title.equals(pageTitile));
+    }
+
+    @AfterTest
+    public void afterTest(){
+        applicationManager.stopAll();
     }
 }
--- src/test/java/com/botmasterzzz/autotest/TestBase.java
+++ src/test/java/com/botmasterzzz/autotest/TestBase.java
@@ -1,24 +1,7 @@
 package com.botmasterzzz.autotest;
 
-import com.botmasterzzz.autotest.helpers.ApplicationManager;
-import org.openqa.selenium.WebDriver;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
-
 public abstract class TestBase {
 
-    protected ApplicationManager applicationManager;
-    protected WebDriver driver;
 
-    @BeforeClass
-    protected void beforeClass(){
-       this.applicationManager = ApplicationManager.getInstance();
-       driver = this.applicationManager.getWebDriverHelper().getDriver();
-    }
-
-    @AfterClass
-    protected void afterClass(){
-        this.applicationManager.stopAll();
-    }
 
 }
